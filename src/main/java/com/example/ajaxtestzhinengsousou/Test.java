f23fabba1d74b6b8f122c5c024d3f836c3416c7e
1069763236@qq.com <Zhouyalei1997>
2024-06-19 02:43:03 +0800

    no-3

--- src/main/java/com/example/ajaxtestzhinengsousou/Test.java
+++ src/main/java/com/example/ajaxtestzhinengsousou/Test.java
@@ -3,5 +3,6 @@ package com.example.ajaxtestzhinengsousou;
 public class Test {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("hello world");
     }
 }
